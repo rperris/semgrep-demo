f9edb8d5803e0f35f29be43ddcc853f323be8f6e
Ron Perris <ron.perris@reddit.com>
2022-03-01 09:48:56 -0700

    add cross-site scripting example

---
+++ src/dangerous.js
@@ -0,0 +1,8 @@
+function createMarkup() {
+  return { __html: "<img src=x onerror=alert(1)/>" };
+}
+
+function TestComponent() {
+  // ruleid:react-dangerouslysetinnerhtml
+  return <div dangerouslySetInnerHTML={createMarkup()} />;
+}
